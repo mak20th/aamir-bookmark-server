0ee0be4661b4cddcded70dc51e5e7ca6dc3c1007
mak20th <mak20th@gmail.com>
2019-07-22 18:48:11 -0500

    completed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 const validateBearerToken = require('./validate-bearer-token');
 const errorHandler = require('./error-handler');
 const bookmarksServer = require('./bookmarks/bookmarks-router');
@@ -21,11 +21,15 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(validateBearerToken);
+app.use(bookmarksServer);
+
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 app.get('/', (req, res) => {
   res.send('Hello, World from Aamir');
 });
-
+app.use(errorHandler);
+module.exports = app;
 
 
--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,6 +1,6 @@
 const myExpress = require('express');
 
-const uuid = require('uuid/v4');// for unique identifier
+const uuid = require('uuid/v4');// for unique identifier 'npm install uuid'
 const logger = require('../logger');//file containing logging tool
 const {bookmarks} = require('../store');
 const {isWebUri} = require('valid-url'); // for web URL validation
@@ -16,6 +16,44 @@ bookmarksRouter
   })
   .post(bodyparser, (req, res)=>{
     //implementation logic goes here~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    for(const field of ['title', 'url','rating']){ // if either title, url or rating is not provided throw an error
+      if(!req.body[field]){
+        logger.error(`${field} is required YO`);
+        return res
+          .status(400)
+          .send(`${field} is required YO`);
+      }
+    }
+    const { title, url, desc, rating } = req.body;
+
+    if(!Number.isInteger(rating) || rating < 0 || rating > 5){ // check if rating is avalid number 0 and 5
+      logger.error(`Invalid rating ${rating} provided`);
+      return res
+        .status(400)
+        .send('Rating must be a number between 0 and 5 YO');
+    }
+    if(!isWebUri(url)){ // validate URL to see if format correct 'npm install valid-url'
+      logger.error(`Invalid url ${url} provided YO`);
+      return res
+        .status(400)
+        .send('You must provide a valid url YO');
+    }
+
+    const newBookmark = {
+      id: uuid(),
+      title, 
+      url, 
+      desc, 
+      rating
+    };
+
+    bookmarks.push(newBookmark);
+    logger.info(`Bookmark ${newBookmark} has been created :)`);
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${newBookmark.id}`)
+      .json(newBookmark);
+
   });
 
 bookmarksRouter
